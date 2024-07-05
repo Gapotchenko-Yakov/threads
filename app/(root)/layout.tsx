a0a95bb2ae38b8887eb351a52df69242f7ce0940
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-05 12:14:55 +0300

    Add empty shared/components.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,9 +30,10 @@ export default function RootLayout({
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
+            <RightSidebar />
           </main>
 
-          <Bottomber />
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
