332a8c293f0b24f7d601ef8c799ab0fd53404c64
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-06 19:06:08 +0300

    Add ahdcn button, for, label components. Add validation for the AccountProfile and UserValidationSchema.

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { UserValidationSchema } from "@/lib/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 interface Props {
   user: {
     id: string;
@@ -13,7 +19,23 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
-  return <div>Account Profile</div>;
+  const form = useForm<z.infer<typeof UserValidationSchema>>({
+    resolver: zodResolver(UserValidationSchema),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserValidationSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return <Form></Form>;
 };
 
 export default AccountProfile;
