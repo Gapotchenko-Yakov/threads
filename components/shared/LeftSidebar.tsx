8f3d8b83ca264c4243f8323aeae4ae5cd05dec6a
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-23 10:46:30 +0300

    Add userId to the '/profile' link.route programatically in the LeftSidebar component.

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,14 +1,17 @@
 "use client";
-import React from "react";
-import { sidebarLinks } from "../../constants";
-import Link from "next/link";
+
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+
+import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -18,11 +21,13 @@ const LeftSidebar = () => {
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
 
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
             >
               <Image
                 src={link.imgURL}
@@ -35,7 +40,8 @@ const LeftSidebar = () => {
           );
         })}
       </div>
-      <div className="mt-10 px-6 max-md:hidden">
+
+      <div className="mt-10 px-6">
         <SignedIn>
           <SignOutButton signOutCallback={() => router.push("/sign-in")}>
             <div className="flex cursor-pointer gap-4 p-4">
