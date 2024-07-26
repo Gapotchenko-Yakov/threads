2779222c3b5f94451bd1296c5d860abf86a90e44
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-26 10:55:16 +0300

    Set next.config ignore ts errors.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     // serverActions: true, // enabled by default in Next.js 14
     serverComponentsExternalPackages: ["mongoose"],
